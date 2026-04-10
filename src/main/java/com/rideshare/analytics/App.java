b59ec4fc7a9d42895c8ba3acbc15b7e0947444ba
subro <subroto.mukherjee1@gmail.com>
2026-04-10 14:25:02 +0100

    Deployment Commit

--- src/main/java/com/rideshare/analytics/App.java
+++ src/main/java/com/rideshare/analytics/App.java
@@ -10,7 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication(scanBasePackages = "com.rideshare.analytics")
 public class App 
 {
-    public static void main( String[] args )
+    public static void main(String[] args)
     {
         SpringApplication.run(App.class, args);
     }
